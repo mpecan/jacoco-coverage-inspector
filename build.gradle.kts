ef031eb866a7729376ce1f73cb47acc51f897b38
Matjaz Domen Pecan <matjaz.pecan@gmail.com>
2025-06-12 22:00:52 +0100

    feat: Integrate SonarQube for code quality analysis and update CI workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.20" apply false
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "io.github.mpecan"
@@ -7,4 +8,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "mpecan_jacoco-coverage-inspector")
+        property("sonar.organization", "mpecan")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
